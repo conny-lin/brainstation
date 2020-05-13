f5d3a816742ce554fc883a756b3d3dba78e929bb
conny-lin <63481030+conny-lin@users.noreply.github.com>
2020-05-13 11:21:53 -0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 my_matrix = np.eye(4)
 my_second_matrix = np.array[[1,2,3,4],
-                            [2,3,4,5],
+                            [2,2,2,2],
                             [5,6,7,8],
                             [6,7,8,9]]
 print(my_matrix)
