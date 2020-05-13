fa607947319fc4095e3d7cb7e8e42e90d5862b22
Conny Lin <connylin318@gmail.com>
2020-05-13 10:41:27 -0700

    created a new python file which prints two matrixes

---
+++ sample.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+my_matrix = np.eye(4)
+my_second_matrix = np.array[[1,2,3,4],
+                            [2,3,4,5],
+                            [5,6,7,8],
+                            [6,7,8,9]]
+print(my_matrix)
+print(my_second_matrix)
+
