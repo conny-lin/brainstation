54445fa8c7bf5e8b87d4d4e4e1385de6d2df12fc
Conny Lin <connylin318@gmail.com>
2020-05-13 11:42:13 -0700

    change third row of matrix 2 first variable to Conny

--- sample.py
+++ sample.py
@@ -5,7 +5,7 @@ my_matrix = np.eye(4)
 my_second_matrix = np.array[[1,2,3,4],
                             [1,1,1,1],
                             [2,2,2,2],
-                            [6,7,8,9]]
+                            ['Conny',7,8,9]]
 print(my_matrix)
 print(my_second_matrix)
 print('change at local computer')
